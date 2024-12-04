f2f16ef9b6b1f7abe4ba13032b5ca12f454be593
Alena <olena.yarmak.mail@gmail.com>
2024-12-04 18:59:20 +0100

    add arrows in slider

--- src/js/app.js
+++ src/js/app.js
@@ -6,6 +6,24 @@ $('.product').slick(
         slidesToShow: 1,
         centerMode: true,
         variableWidth: true,
-        arrows: false
+        arrows: true,
+        autoplay: true,
+        nextArrow: '<img class="custom-arrow custom-arrow-right" src="./img/arrow.png"></img>',
+        prevArrow: '<img class="custom-arrow custom-arrow-left" src="./img/arrow.png"></img>',
       }
 );
+
+$('.slick-slider').on('afterChange', function(event, slick, currentSlide) {
+  // Сбрасываем стили у всех элементов
+  document.querySelectorAll('.slick-slide .product__item').forEach(item => {
+    item.style.border = 'none';
+  });
+
+  // Применяем стиль к активному элементу
+  document.querySelectorAll('.slick-slide.slick-active').forEach(activeSlide => {
+    const productItem = activeSlide.querySelector('.product__item');
+    if (productItem) {
+      productItem.style.border = '1px solid #F06E2D';
+    }
+  });
+});
